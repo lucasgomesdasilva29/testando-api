35c1e3ed7bcec2360098ac34a57e3c5d3c927b27
Lucas Gomes da Silva <lucasgomesdasilva29@gmail.com>
2022-10-22 15:32:22 -0300

    Projeto de teste criado

---
+++ src/main/java/br/com/servicetech/TestandoApi/App.java
@@ -0,0 +1,13 @@
+package br.com.servicetech.TestandoApi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
